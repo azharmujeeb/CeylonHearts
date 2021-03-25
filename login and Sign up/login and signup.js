b4e67fe55fb944e1589be46fc4de851e6cdf5b95
Irusha Samarawickrama <irusha.is10@gmail.com>
2021-03-26 05:24:30 +0530

    firebase

--- login and Sign up/login and signup.js
+++ login and Sign up/login and signup.js
@@ -1,18 +1,3 @@
-  
-  //website firebase config below
-  var firebaseConfig = {
-    apiKey: "AIzaSyAa7iEF-LLXz0b4SsfxdrirbXnwLhvDonk",
-    authDomain: "ceylonheartswebsite2.firebaseapp.com",
-    databaseURL: "https://ceylonheartswebsite2-default-rtdb.firebaseio.com",
-    projectId: "ceylonheartswebsite2",
-    storageBucket: "ceylonheartswebsite2.appspot.com",
-    messagingSenderId: "212125160276",
-    appId: "1:212125160276:web:8129a7e527bce720582ec1",
-    measurementId: "G-H4LS675FYM"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth();
 function signup(){
 $(document).ready(()=>{
         // Do stuff here, including _calling_ codeAddress(), but not _defining_ it!
@@ -24,8 +9,8 @@ $(document).ready(()=>{
     
     const promise = auth().createUserWithEmailAndPassword( email.value, password.value);
     promise.catch(e => alert(e.message));
-    
+    alert("Signed Up");
  
 })
 }
-alert("Signed Up");
+
