057bf3e76c3a74a94d8fefa7f4b27754f5da1241
Irusha Samarawickrama <irusha.is10@gmail.com>
2021-03-27 18:02:13 +0530

    firebase for sign up and login

--- FIREBASE/login and Sign up/login and signup.js
+++ FIREBASE/login and Sign up/login and signup.js
@@ -45,17 +45,20 @@ function submitForm(e) {
    let samCard = document.querySelector(".Scard").value;
    let cause = document.querySelector(".custom-select").value;
    let picture = document.querySelector(".pic").value;
+   let reason = document.querySelector(".reason").value;
+   
 
 
-   console.log(fullname, email, password,phone,IDnum,samCard,cause,picture);
 
-   saveContactInfo(fullname, email, password, phone, IDnum,samCard,cause,picture);
+   console.log(fullname, email, password,phone,IDnum,samCard,cause,picture,reason);
+
+   saveContactInfo(fullname, email, password, phone, IDnum,samCard,cause,picture,reason);
 
    document.querySelector(".signup-form").reset(); 
 
 }
    // Save infos to Firebase
-function saveContactInfo(fullname, email, password,phone,IDnum,samCard,cause,picture) {
+function saveContactInfo(fullname, email, password,phone,IDnum,samCard,cause,picture,reason) {
   let newSignupInfo = signupInfo.push();
 
   newSignupInfo.set({
@@ -67,7 +70,59 @@ function saveContactInfo(fullname, email, password,phone,IDnum,samCard,cause,pic
     samCard:samCard,
     cause:cause,
     picture:picture,
+    reason:reason,
   });
   window.alert("success!");
  
-}
+
+  };
+
+  // login firebase 
+
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+  
+      document.getElementById("user_div").style.display = "block";
+      document.getElementById("login_div").style.display = "none";
+  
+      var user = firebase.auth().currentUser;
+  
+      if(user != null){
+  
+        var email_id = user.email;
+        document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+  
+      }
+  
+    } else {
+      // No user is signed in.
+  
+      document.getElementById("user_div").style.display = "none";
+      document.getElementById("login_div").style.display = "block";
+  
+    }
+  });
+  
+  function login(){
+  
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+  
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+  
+      window.alert("Error : " + errorMessage);
+  
+      // ...
+    });
+  
+  }
+  
+  function logout(){
+    firebase.auth().signOut();
+  }
+
+
